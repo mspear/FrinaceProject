cb23667a8457fd0c4b11fa114583fa829b01809e
Michael Spear <mwspear@gmail.com>
2016-04-05 11:14:59 -0500

    Initial commit

---
+++ app/src/main/java/cachemoney420/frinaceproject/FinaceActivity.java
@@ -0,0 +1,13 @@
+package cachemoney420.frinaceproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finace);
+    }
+}
